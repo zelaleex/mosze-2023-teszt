ea2732168e4f2fe08ee6a6188e7261818523267b
zelaleex <zelko.alex99@gmail.com>
2023-09-24 21:19:29 +0200

    hiba-javitva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    //int *b = new int[NELEMENTS];
-    //std::cout << '1-100 ertekek duplazasa'
-    //for (int i = 0;)
+    int b[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        b[i] = (i+1) * 2;
     }
-    //for (int i = 0; i; i++)
+
+    std::cout << "Ertek:" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        //std::cout << "Ertek:"
+        std::cout << b[i];
     }    
+    std::cout << std::endl;
+    
     std::cout << "Atlag szamitasa: " << std::endl;
-    //int atlag;
+    double atlag = 0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        //atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
